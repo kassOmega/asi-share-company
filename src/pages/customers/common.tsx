60e2cdcf9cf572a974e2f0623e4fad2fb6d6c3a8
kassahun <kass3me@gmail.com>
2023-06-22 15:09:35 +0300

    fix:error fixed

--- src/pages/customers/common.tsx
+++ src/pages/customers/common.tsx
@@ -131,8 +131,9 @@ export function SimpleDialog(props: SimpleDialogProps) {
 
     const capitalizedFirstName =
       firstName.charAt(0).toUpperCase() + firstName.slice(1);
-    const capitalizedLastName =
-      lastName.charAt(0).toUpperCase() + lastName.slice(1);
+    const capitalizedLastName = lastName
+      ? lastName.charAt(0).toUpperCase() + lastName.slice(1)
+      : "";
 
     return `${capitalizedFirstName} ${capitalizedLastName}`;
   }
